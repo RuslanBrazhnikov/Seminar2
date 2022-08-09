5ecd0ed2baa43273d572742aaa3077eb060d9a1c
ruslan brazhnikov <ruslan.1995mai@gmail.com>
2022-08-09 12:28:42 +0500

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿int number = new Random().Next(10, 100);
+Console.WriteLine($"Случайное число из отрезка 10 - 99 -> {number}");
+
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+
+if (firstDigit > secondDigit)
+{
+    Console.Write($"Наибольшая цифра числа --> {firstDigit}");
+}
+else if (firstDigit == secondDigit)
+{
+    Console.Write($"Обе цифры числа равны!");
+}
+else
+{
+    Console.Write($"Наибольшая цифра числа --> {secondDigit}");
+}
