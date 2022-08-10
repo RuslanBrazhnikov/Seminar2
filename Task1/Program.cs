ba566e76a2e9acfcd82768b86a6231b07471c4c2
ruslan brazhnikov <ruslan.1995mai@gmail.com>
2022-08-10 13:48:31 +0500

    Task3

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,18 +1,34 @@
 ﻿int number = new Random().Next(10, 100);
 Console.WriteLine($"Случайное число из отрезка 10 - 99 -> {number}");
 
-int firstDigit = number / 10;
-int secondDigit = number % 10;
+// int firstDigit = number / 10;
+// int secondDigit = number % 10;
 
-if (firstDigit > secondDigit)
+// if (firstDigit > secondDigit)
+// {
+//     Console.Write($"Наибольшая цифра числа --> {firstDigit}");
+// }
+// else if (firstDigit == secondDigit)
+// {
+//     Console.Write($"Обе цифры числа равны!");
+// }
+// else
+// {
+//     Console.Write($"Наибольшая цифра числа --> {secondDigit}");
+// }
+
+// int res = firstDigit > secondDigit ? firstDigit : secondDigit;
+// Console.WriteLine(res);
+
+int MaxDigit(int num)
 {
-    Console.Write($"Наибольшая цифра числа --> {firstDigit}");
+    int firstDigit = number / 10;
+    int secondDigit = number % 10;
+
+    return firstDigit > secondDigit ? firstDigit : secondDigit;
 }
-else if (firstDigit == secondDigit)
-{
-    Console.Write($"Обе цифры числа равны!");
-}
-else
-{
-    Console.Write($"Наибольшая цифра числа --> {secondDigit}");
-}
+
+int result = MaxDigit(88);
+string res = result == -1 ? "Цифры одинкаовы" : result.ToString();
+Console.Write($"Наибольшая цифра числа {number}--> {res}");
+
---
+++ Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу которая принимает рандомное число и удаляет вторую цифру
+
+
+int number = new Random().Next(100, 1000);
+
+int RemoveSecons(int num)
+{
+    int number1 = num / 100;
+    int number2 = num % 10;
+    int nov = number1 * 10 + number2;
+    return nov;
+}
+
+int result = RemoveSecons(number);
+Console.Write($"{number} ==> {result}");
+
